9fb75b52065994ac9d5b827c534546c627a1aad7
marco <marco@DESKTOP-CJTTCG2>
2023-08-30 11:07:59 -0300

    Adicionar arquivos de projeto.

---
+++ exercicioClasse02/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Globalization;
+namespace exercicioClasse02
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public double Quantidade;
+
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto()
+        {
+            Quantidade += double.Parse(Console.ReadLine());
+        }
+        public void RemoverProduto()
+        {
+            Quantidade -= double.Parse(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, ${Preco.ToString(CultureInfo.InvariantCulture)}, {Quantidade} unidades, total: $: {ValorTotal().ToString(CultureInfo.InvariantCulture)}";
+        }
+    }
+}
+
---
+++ exercicioClasse02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using exercicioClasse02;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Produto p = new Produto();
+        
+        Console.WriteLine("Coloque os dados do produto:");
+        Console.Write("Nome: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Preco: ");
+        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        p.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine($"Dados do produto: {p}"); 
+        Console.Write("Digite o numero de produtos a serem adicionados: ");
+        p.AdicionarProduto();       
+        Console.WriteLine($"Dados atualizados: {p}");
+        Console.Write("Digite o numero de produtos a serem removidos: ");
+        p.RemoverProduto();
+        Console.WriteLine($"Dados atualizados: {p}");
+    }
+}
